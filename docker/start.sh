476ea07c783569ef24f58590d2ca71a7cd9a3ced
Toriq Habil Fadhila <toriqqhabilf12@gmail.com>
2026-04-14 19:31:58 +0700

    Enhance start.sh with Laravel setup and improve nginx.conf configuration

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+set -e
 
-# Start PHP-FPM in background
+# Generate APP_KEY jika belum ada
+if [ -z "$APP_KEY" ]; then
+    php artisan key:generate --force
+fi
+
+# Laravel setup
+php artisan config:cache
+php artisan route:cache
+php artisan migrate --force
+
+# Start PHP-FPM background
 php-fpm -D
 
-# Start Nginx in foreground
+# Start Nginx foreground (port 80)
 nginx -g "daemon off;"
