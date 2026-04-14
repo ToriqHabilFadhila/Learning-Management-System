5fb8f51d1603e04c47f80d79768d247d6305e017
Toriq Habil Fadhila <toriqqhabilf12@gmail.com>
2026-04-15 05:12:34 +0700

    CSS POLOS FIX

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setLocale('id');
+
+        if (config('app.env') === 'production' || $this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
